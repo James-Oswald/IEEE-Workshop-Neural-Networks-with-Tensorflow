6bac2ec1129f14e9c9935f9c50a17772adac453b
James-Oswald <jamesoswald111@gmail.com>
2020-11-12 18:59:26 -0500

    Added networks

---
+++ CnnNetwork.py
@@ -0,0 +1,50 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  #disable Tensorflow warning messages
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# the data, split between train and test sets
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+train_images = train_images.astype("float32") / 255
+test_images = test_images.astype("float32") / 255
+
+# Make sure images have shape (28, 28, 1)
+train_images = np.expand_dims(train_images, -1)
+test_images = np.expand_dims(test_images, -1)
+print("train_images shape:", train_images.shape)
+print(train_images.shape[0], "train samples")
+print(test_images.shape[0], "test samples")
+
+# convert class vectors to binary class matrices
+train_labels = keras.utils.to_categorical(train_labels, num_classes)
+test_labels = keras.utils.to_categorical(test_labels, num_classes)
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+model.summary()
+
+batch_size = 128
+epochs = 3
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(test_images, test_labels, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
